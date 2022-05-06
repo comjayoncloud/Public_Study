16a5fa572bf0a51f0ff186d41d3d1169c3b69718
jaewoo <comjayoncloud@gmail.com>
2022-05-06 14:11:18 +0900

    2022.05.06 ver2

--- LOL_Project_practice/frontend/State_example.js
+++ LOL_Project_practice/frontend/State_example.js
@@ -1,4 +1,4 @@
-// class 로 생성시
+//// - class 로 생성시 (component)
 
 class Car extends React.Component {
   render() {
@@ -6,13 +6,13 @@ class Car extends React.Component {
   }
 }
 
-// function 로 생성시
+//// - function 로 생성시 (component)
 
 function Car() {
   return <h2>Hi, I am a Car!</h2>;
 }
 
-// state
+//// - state
 
 class Car extends React.Component {
   constructor() {
@@ -24,7 +24,7 @@ class Car extends React.Component {
   }
 }
 
-// prop
+//// - prop
 
 class Car extends React.Component {
   render() {
@@ -34,7 +34,7 @@ class Car extends React.Component {
 
 ReactDOM.render(<Car color="red" />, document.getElementById("root"));
 
-// 컴포넌트 안에 다른 컴포넌트 포함
+//// - 컴포넌트 안에 다른 컴포넌트 포함
 
 class Hello extends React.Component {
   render() {
@@ -50,7 +50,7 @@ class World extends React.Component {
 
 ReactDOM.render(<World />, document.getElementById("root"));
 
-// 다른 파일(.js)에 있는 컴포넌트를 포함
+//// - 다른 파일(.js)에 있는 컴포넌트를 포함
 
 // HelloFile.js
 
@@ -63,3 +63,81 @@ class Hello extends React.Component {
 }
 
 export default Hello;
+
+// app.js
+
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Hello from './HelloFile';	// <== 여기 추가
+
+function App() {
+  let hello_h1 = <h1>Hello World~</h1>;
+  return (
+    <div className="App">
+      {hello_h1}
+      <Hello name="bryan" />		// <== 이렇게 사용
+    </div>
+  );
+}
+
+export default App;
+
+//// - state를 사용하려면 constructor에서 속성을 초기화
+
+class Hello extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        gender: "man",
+        tall: "181",
+        weight: "80"
+      };
+    }
+    render() {
+      return (
+        <div>
+          <h2>Hello {this.props.name}!</h2>
+          <p>your gender is {this.state.gender}</p>
+          <p>your tall is {this.state.tall} cm</p>
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<Hello name="bryan"/>, document.getElementById('root'));
+
+
+//// - state 수정 :  this.setState() 
+
+class Hello extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        gender: "man",
+        tall: "181",
+        weight: "80"
+      };
+    }
+    changeGender = () =>{
+      if( this.state.gender == "man")
+        this.setState({gender:"woman"})
+      else
+        this.setState({gender:"man"})
+    }
+    render() {
+      return (
+        <div>
+          <h2>Hello {this.props.name}!</h2>
+          <p>your gender is {this.state.gender}</p>
+          <p>your tall is {this.state.tall} cm</p>
+          <button type="button" onClick={this.changeGender}>Gender Change</button>
+        </div>
+      );
+    }
+  }
+  
+  ReactDOM.render(<Hello name="bryan"/>, document.getElementById('root'));
+
+// 화면을 보면 man으로 되어있음. 버튼을 누러서 바꾸는 코드. 중요한것은 changeGender가 바로 실행안됨
+
--- LOL_Project_practice/frontend/props_example.js
+++ LOL_Project_practice/frontend/props_example.js
@@ -1,4 +1,14 @@
-//// props는 읽기전용!
+//// - prop
+
+class Car extends React.Component {
+  render() {
+    return <h2>I am a {this.props.color} Car!</h2>;
+  }
+}
+
+ReactDOM.render(<Car color="red" />, document.getElementById("root"));
+
+//// - props는 읽기전용!
 
 class Hello extends React.Component {
   render() {
@@ -10,7 +20,7 @@ ReactDOM.render(<Hello name="bryan" />, document.getElementById("root"));
 
 // this.props.name = 'ok google'; 하면 오류!
 
-//// 파라메터로 props값 전달
+//// - 파라메터로 props값 전달
 
 const param_name = "bryan";
 
@@ -22,4 +32,31 @@ class Hello extends React.Component {
 
 ReactDOM.render(<Hello name={param_name} />, document.getElementById("root"));
 
-////
+//// - object 값으로 전달
+
+const param_name = { first: "bryan", last: "oh" }; // 이렇게 object 형식으로 바꾸면
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <h2>
+        Hello {this.props.name.first} {this.props.name.last}!
+      </h2>
+    ); // 이렇게 사용.
+  }
+}
+
+ReactDOM.render(<Hello name={param_name} />, document.getElementById("root"));
+
+//// - constructor 로 props 전달.
+
+class Hello extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h2>Hello {this.props.name}!</h2>;
+  }
+}
+
+ReactDOM.render(<Hello name="bryan" />, document.getElementById("root"));
